8ab7482c50ea3184267fa4f5c56ef900a4e5e96e
Sumanta Kabiraj <sumantablog@gmail.com>
2022-12-22 16:30:29 +0530

    client setup with server

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import { Router, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Router>
+    </>
+  );
+}
+
+export default App;
